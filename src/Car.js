407efc4dac229281184185763e71d3d50cb8583c
d0422 <rlfehd2013@naver.com>
2022-10-20 14:03:55 +0900

    Feat:랜덤값을 발생시켜 4이상이면 앞으로 전진, 아니면 정지

--- src/Car.js
+++ src/Car.js
@@ -9,4 +9,14 @@ export default class Car {
     this.state = getState(this.state, randomNumber);
     displayState();
   }
+  getRandomNumber() {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+    return randomNumber;
+  }
+  getState(state, randomNumber) {
+    if (randomNumber >= 4) {
+      state = state + 1;
+    }
+    return state;
+  }
 }
